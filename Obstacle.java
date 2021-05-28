71aa99f2322965136cc8618d4f9f3bb53cba4a6c
shanna2023 <62947101+shanna2023@users.noreply.github.com>
2021-05-28 16:55:34 -0400

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
@@ -96,12 +97,120 @@ public class Obstacle extends Entity
 		}
 	}
 
-	public Tile hitBy(Bullet b)
+	/*
+	public Tile hitBy(Bullet b, SpaceInvadersMain s)
 	{
 		if (b.isTouching(this))
 		{
-
 			Point contact = new Point((b.getX() * 2 + b.getWidth())/2 - getX(), b.getY() - getY());
+			for (int i = 0; i < obstacle.length; i++)
+			{
+				for (int j = 0; j < obstacle[i].length; j++)
+				{
+					if (obstacle[i][j] != null)
+					{
+//						if (obstacle[i][j].contains(contact) && obstacle[i][j].getState() == SOLID)
+//						{
+//							obstacle[i][j].setState(BROKEN);
+//							return obstacle[i][j];
+//						}
+//						if (i == 0 && j == 2 && obstacle[0][2].contains(contact, true))
+//						{
+//							System.out.println("BRUHHHHH");
+//							System.out.println(((Corner)obstacle[0][2]).thingy(contact));
+//						}
+						if (obstacle[i][j] instanceof Corner)
+						{
+							Corner corner = (Corner)obstacle[i][j];
+							Polygon triangle = corner.getTriangle();
+							if (b.getDx() == -10)
+							{
+								if (corner.getType() == Corner.TL)
+								{
+									if (b.isInsideOf(corner))
+									{
+										if (corner.getX() + corner.getWidth() - b.getX() >= b.getWidth() / 2 || obstacle[i][j+1] == null)
+										{
+											obstacle[i][j].setState(BROKEN);
+											return obstacle[i][j];
+										}
+										else if (obstacle[i][j+1] != null)
+										{
+											obstacle[i][j+1].setState(BROKEN);
+											return obstacle[i][j+1];
+										}
+									}
+								}
+								else if (corner.getType() == Corner.TR)
+								{
+									if (b.isInsideOf(corner))
+									{
+										if (corner.getX() - b.getX() < b.getWidth() / 2 || obstacle[i][j-1] == null)
+										{
+											obstacle[i][j].setState(BROKEN);
+											return obstacle[i][j];
+										}
+										else if (obstacle[i][j-1] != null)
+										{
+											obstacle[i][j-1].setState(BROKEN);
+											return obstacle[i][j-1];
+										}
+									}
+								}
+								else if (corner.getType() == Corner.BR)
+								{
+									if (contact.x < corner.getX() + corner.getWidth())
+									{
+										double xDist = corner.getX() + corner.getWidth() - contact.x;
+										double yDist = xDist/3;
+										if (contact.y < corner.getY() + corner.getHeight() - yDist)
+										{
+											obstacle[i][j].setState(BROKEN);
+											return obstacle[i][j];
+										}
+									}
+									else
+									{
+										
+									}
+								}
+								else
+								{
+									
+								}
+							}
+							else
+							{
+								if (corner.getType() == Corner.BL || corner.getType() == Corner.BR)
+								{
+									if (b.isInsideOf(corner))
+									{
+										obstacle[i][j].setState(BROKEN);
+										return obstacle[i][j];
+									}
+								}
+							}
+						}
+						else
+						{
+							if (b.isInsideOf(obstacle[i][j]) && obstacle[i][j].getState() == SOLID)
+							{
+								obstacle[i][j].setState(BROKEN);
+								return obstacle[i][j];
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+	*/
+	
+	public Tile[] hitBy(Bullet b)
+	{
+		if (b.isTouching(this))
+		{
 			for (int i = 0; i < obstacle.length; i++)
 			{
 
@@ -111,8 +220,14 @@ public class Obstacle extends Entity
 					if (obstacle[i][j] != null && obstacle[i][j].getState() == SOLID)
 					{
 						System.out.println("h213");
-						Rectangle rec = new Rectangle(b.getX() - this.getX(), b.getY() - this.getY(), b.getWidth(), b.getHeight());			
-						if(obstacle[i][j].getS().intersects(rec))
+						Rectangle rec = new Rectangle(b.getX() - this.getX(), b.getY() - this.getY(), b.getWidth(), b.getHeight());
+						Polygon poly = new Polygon(obstacle[i][j].getS().xpoints, obstacle[i][j].getS().ypoints, obstacle[i][j].getS().npoints);
+						poly.translate(obstacle[i][j].getX(), obstacle[i][j].getY());
+						System.out.println(rec.getBounds());
+						System.out.println(b.getBounds());
+						System.out.println(this.getBounds());
+						System.out.println(obstacle[i][j].getS().getBounds());
+						if(poly.intersects(rec))
 						{
 							System.out.println(i + " " + j);
 							if(j + 1 < obstacle[i].length && obstacle[i][j+1] != null && obstacle[i][j+1].getS().intersects(rec))
@@ -120,44 +235,21 @@ public class Obstacle extends Entity
 								System.out.println("h1");
 								obstacle[i][j].setState(BROKEN);
 								obstacle[i][j+1].setState(BROKEN);
-								return obstacle[i][j];
+								return new Tile[] {obstacle[i][j], obstacle[i][j+1]};
 							}
 							else
 							{
 								System.out.println("h2");
 								obstacle[i][j].setState(BROKEN);
-								return obstacle[i][j];
+								return new Tile[] {obstacle[i][j]};
 							}
 						}
-
 					}
 				}
 			}
 		}
 		return null;
 	}
-
-	//	public void test(Tile t)
-	//	{
-	//		if (t != null)
-	//		{
-	//			for (int i = 0; i < t.getNeighbors().size(); i++)
-	//			{
-	//				if (t.getNeighbors().get(i) != null)
-	//				{	
-	//					if (t.getNeighbors().get(i).isGone())
-	//					{
-	//						obstacle[t.getNeighbors().get(i).getR()][t.getNeighbors().get(i).getC()] = null;
-	//					}
-	//				}
-	//			}
-	//			if (t.isGone())
-	//			{
-	//				obstacle[t.getR()][t.getC()] = null;
-	//			}
-	//		}
-	//		repaint();
-	//	}
 
 	public void test(Tile t)
 	{
@@ -169,7 +261,7 @@ public class Obstacle extends Entity
 			{
 				System.out.println(2);
 				if (t.getNeighbors().get(i) != null)
-				{
+				{	
 					System.out.println(3);
 					if (t.getNeighbors().get(i).isGone(t))
 					{
@@ -184,49 +276,13 @@ public class Obstacle extends Entity
 				obstacle[t.getR()][t.getC()].setState(DESTROYED);;
 			}
 		}
-		System.out.println("*");
+		System.out.println("*********************");
 		repaint();
 	}
 
 	@Override
 	public void paintComponent(Graphics g)
 	{
-		//		Graphics2D g2 = (Graphics2D)g;
-		//		g2.setColor(Color.GREEN);
-		//		for (int r = 0; r < obstacle.length; r++)
-		//		{
-		//			for (int c = 0; c < obstacle[r].length; c++)
-		//			{
-		//				if (obstacle[r][c] != null)
-		//				{
-		//					if (obstacle[r][c].getState() == SOLID)
-		//					{
-		//						this.add(obstacle[r][c]);
-		//					}
-		//					else if (obstacle[r][c].getState() == BROKEN)
-		//					{
-		//						this.remove(obstacle[r][c]);
-		//						repaint();
-		//						if (obstacle[r][c].getBroken() == null)
-		//						{
-		//							int num = (int)(Math.random()*5);
-		//							obstacle[r][c].setBroken(broken[num]);
-		//						}
-		//						for (int i = 0; i < obstacle[r][c].getBroken().length; i++)
-		//						{
-		//							for (int j = 0; j < obstacle[r][c].getBroken()[i].length; j++)
-		//							{
-		//								if (obstacle[r][c].getBroken()[i][j])
-		//								{
-		//									g2.fillRect((int)(obstacle[r][c].getX() + j), (int)(obstacle[r][c].getY() + i), 1, 1);
-		//								}
-		//							}
-		//						}
-		//					}
-		//				}
-		//			}
-		//		}
-		//	}
 		Graphics2D g2 = (Graphics2D)g;
 		g2.setColor(Color.GREEN);
 		for (int r = 0; r < obstacle.length; r++)
@@ -271,5 +327,3 @@ public class Obstacle extends Entity
 		}
 	}
 }
-
-
