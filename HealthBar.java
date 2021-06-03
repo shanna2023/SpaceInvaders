23414d1ec4ec1c70951505d59914971be51815b6
dtx14 <dtx14@DESKTOP-83GJR3S>
2021-06-02 20:56:11 -0400

    Arnav is beautiful

--- HealthBar.java
+++ HealthBar.java
@@ -29,8 +29,6 @@ public class HealthBar extends JComponent
 	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D)g;
-		g2.setColor(Color.WHITE);
-		g2.drawRoundRect(1, 1, 198, 23, 10, 10);
 		g2.setColor(new Color(169, 3, 252));
 		g2.fillRoundRect(1, 1, health - 2, 23, 10, 10);
 	}
